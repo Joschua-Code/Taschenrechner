9cf9631335acccaf1778e8f1aaa745b4aac8e647
Joschua <joschua.info@web.de>
2025-11-13 16:34:05 +0100

    Git Notifikation Testen

--- Taschenrechner.py
+++ Taschenrechner.py
@@ -4,22 +4,23 @@ import sys
 
 window = tk.Tk()
 window.title("Taschenrechner")
-window.geometry("500x600")
+window.geometry("400x550")
 window.minsize(width=250,height=300)
-window.grid_columnconfigure((0, 1, 2, 3, 4), weight=1, uniform="cols")
+window.configure(padx= 50, pady=50)
+#window.grid_columnconfigure((0, 1, 2, 3, 4), weight=1, uniform="cols")
 
-
+ergebnisfenster = tk.Frame(window)
+ergebnisfenster.grid(row=0, column=0, columnspan=4, sticky="ew")
+ergebnisfenster.grid_columnconfigure((0,1,2,3,4), weight=1, uniform="cols")
 
 #Taschenrechner
 
-#Zeile 1 (Zahl1 - Operator - Zahl2 - Ergebnis)
-
-
+#Ergebnisfenster (Zahl1 - Operator - Zahl2 - Ergebnis)
 #Erste Zahl
 zahl1 = tk.IntVar()
 zahl1.set(1)
 
-zahl1_label = ttk.Label(window)
+zahl1_label = ttk.Label(ergebnisfenster)
 zahl1_label.grid(row=0, column=0)
 zahl1_label.configure(textvariable=zahl1)
 
@@ -28,7 +29,7 @@ zahl1_label.configure(textvariable=zahl1)
 operator = tk.StringVar()
 operator.set("+")
 
-operator_label = ttk.Label(window)
+operator_label = ttk.Label(ergebnisfenster)
 operator_label.grid(row=0, column=1)
 operator_label.configure(textvariable=operator)
 
@@ -37,32 +38,69 @@ operator_label.configure(textvariable=operator)
 zahl2 = tk.IntVar()
 zahl2.set(2)
 
-zahl2_label = ttk.Label(window)
+zahl2_label = ttk.Label(ergebnisfenster)
 zahl2_label.grid(row=0, column=2)
 zahl2_label.configure(textvariable=zahl2)
 
 
 #Gleichzeichenlabel
+gleichzeichen_label = ttk.Label(ergebnisfenster)
+gleichzeichen_label.grid(row=0, column=3)
+
 gleichzeichen = tk.StringVar()
 gleichzeichen.set("=")
-
-gleichzeichen_label = ttk.Label(window)
-gleichzeichen_label.grid(row=0, column=3)
 gleichzeichen_label.configure(textvariable=gleichzeichen)
-
 
 #Ergebnislabel
 ergebnis = tk.IntVar()
 ergebnis.set(3)
 
-ergebnis_label = ttk.Label(window)
+ergebnis_label = ttk.Label(ergebnisfenster)
 ergebnis_label.grid(row=0, column=4)
 ergebnis_label.configure(textvariable=ergebnis)
 
 
+#Abstandshalter
+ergebnis_label = ttk.Label(window)
+ergebnis_label.grid(row=1, column=0)
+ergebnis_label.configure(textvariable="")
 
 
 
+
+#ClearButton
+clear = ttk.Button(
+    window, 
+    text="C", 
+    #command=lambda: lzahl1_int.set(lzahl1_int.get()+1)
+    )
+clear.grid(row=3, column=0)
+
+#back_button
+back = ttk.Button(
+    window, 
+    text="⌫", 
+    #command=lambda: lzahl1_int.set(lzahl1_int.get()+1)
+    )
+back.grid(row=3, column=1)
+
+
+#History button
+history = ttk.Button(
+    window, 
+    text="☰", 
+    #command=lambda: lzahl1_int.set(lzahl1_int.get()+1)
+    )
+history.grid(row=3, column=2)
+
+
+#Divide button
+help = ttk.Button(
+    window, 
+    text="?", 
+    #command=lambda: lzahl1_int.set(lzahl1_int.get()+1)
+    )
+help.grid(row=3, column=3)
 
 
 
